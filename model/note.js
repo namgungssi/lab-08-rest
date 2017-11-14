154a573e72e24736879eda7d7563776f48b54659
dalhun namgung <bnamgung@gmail.com>
2017-11-14 14:29:02 -0800

    put

---
+++ model/note.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+const uuid = require ('uuid/v1');
+
+class Note {
+  constructor (config) {
+    this.id = uuid ();
+    this.createdOn = new Date ();
+    this.title = config.title || ' ';
+    this.content = config.content || ' ';
+  }
+};
+
+
+
+
+
+module.exports = Note;
--- server.js
+++ server.js
@@ -3,3 +3,7 @@
 
 require ('dotenv').config ();
 require ('./lib/_server').start ();
+
+//server.start (process.env.PORT)
+//.then (console.log);
+//.catch (console.log);
