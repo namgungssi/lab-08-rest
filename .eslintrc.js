06be34ce9a9776ab7d7612ec260c8d1e5c288a06
dalhun namgung <bnamgung@gmail.com>
2017-11-14 21:28:50 -0800

    sdfhdksjfds

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     "env": {
         "browser": true
+        "es6": true,
+        "node": true,
+        "browser": true,
+        "jest":true
     },
     "extends": "eslint:recommended",
     "rules": {
